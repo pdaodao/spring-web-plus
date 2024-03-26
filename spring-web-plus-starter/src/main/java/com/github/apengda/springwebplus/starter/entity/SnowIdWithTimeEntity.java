3546557b9ada8760eaec771177899580b27df918
apengda <damengpeng@163.com>
2024-03-26 10:58:06 +0800

    [fix] data

--- spring-web-plus-starter/src/main/java/com/github/apengda/springwebplus/starter/entity/SnowIdWithTimeEntity.java
+++ spring-web-plus-starter/src/main/java/com/github/apengda/springwebplus/starter/entity/SnowIdWithTimeEntity.java
@@ -4,10 +4,12 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
+@Data
 public class SnowIdWithTimeEntity extends SnowIdEntity {
     @TableField(value = "create_time", fill = FieldFill.INSERT)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
