1622f63c6bcee1cf51242dabab28847c80bc3921
apengda <damengpeng@163.com>
2024-11-06 16:51:03 +0800

    [frame] page and list result structure

--- spring-web-plus-base/src/main/java/com/github/pdaodao/springwebplus/base/config/RestResponseAdvice.java
+++ spring-web-plus-base/src/main/java/com/github/pdaodao/springwebplus/base/config/RestResponseAdvice.java
@@ -1,6 +1,7 @@
 package com.github.pdaodao.springwebplus.base.config;
 
 import com.github.pdaodao.springwebplus.base.pojo.IResponse;
+import com.github.pdaodao.springwebplus.base.pojo.PageResult;
 import com.github.pdaodao.springwebplus.base.pojo.RestCode;
 import com.github.pdaodao.springwebplus.base.pojo.RestResponse;
 import com.github.pdaodao.springwebplus.base.util.JsonUtil;
@@ -17,12 +18,27 @@ import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
+
 /**
  * 接口返回值封装
  */
 @RestControllerAdvice
 @ConditionalOnProperty(value = "global.restResponse", havingValue = "true", matchIfMissing = true)
 public class RestResponseAdvice implements ResponseBodyAdvice<Object> {
+
+    private static boolean isCollectionReturnType(MethodParameter methodParameter) {
+        final Type returnType = methodParameter.getGenericParameterType();
+        if (returnType instanceof ParameterizedType) {
+            Type rawType = ((ParameterizedType) returnType).getRawType();
+            if (rawType instanceof Class<?>) {
+                return Collection.class.isAssignableFrom((Class<?>) rawType);
+            }
+        }
+        return false;
+    }
 
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
@@ -37,10 +53,19 @@ public class RestResponseAdvice implements ResponseBodyAdvice<Object> {
 
     @Nullable
     @Override
-    public Object beforeBodyWrite(@Nullable final Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+    public Object beforeBodyWrite(@Nullable Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
         if (body == null && returnType.getGenericParameterType().getTypeName().equals("void")) {
             return RestResponse.success(body);
         }
+        // 保持分页接口 和 列表接口 格式一致
+        if (isCollectionReturnType(returnType)) {
+            final PageResult p = new PageResult();
+            if (body != null) {
+                p.setList((Collection) body);
+            }
+            body = p;
+        }
+
         final IResponse restResponse = body instanceof IResponse ? (IResponse) body : RestResponse.success(body);
         if (restResponse instanceof RestResponse) {
             RestResponse r = (RestResponse) restResponse;
--- spring-web-plus-base/src/main/java/com/github/pdaodao/springwebplus/base/pojo/PageResult.java
+++ spring-web-plus-base/src/main/java/com/github/pdaodao/springwebplus/base/pojo/PageResult.java
@@ -1,13 +1,20 @@
 package com.github.pdaodao.springwebplus.base.pojo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import java.util.Collection;
 import java.util.List;
 
 @Data
 @Schema(description = "接口返回分页数据封装")
-public class PageResult<T> extends RestResponse<List<T>> {
+public class PageResult<T> {
+    @Schema(description = "数据")
+    private Collection<T> list;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Schema(description = "分页数据")
     private PageInfo pageInfo;
 
     public static <T> PageResult<T> build(Long pageNum, Long pageSize, Long total, List<T> data) {
@@ -16,7 +23,7 @@ public class PageResult<T> extends RestResponse<List<T>> {
         pageInfo.setPageSize(pageSize);
         pageInfo.setTotal(total);
         final PageResult pageR = new PageResult();
-        pageR.setData(data);
+        pageR.setList(data);
         pageR.setPageInfo(pageInfo);
         return pageR;
     }
