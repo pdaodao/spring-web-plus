bdb8846835c4f12f6533efd3143aa01135b6d911
apengda <damengpeng@163.com>
2025-02-05 13:50:03 +0800

    drop flow module

--- spring-web-plus-flow/src/main/java/com/github/pdaodao/springwebplus/bflow/mapper/BfInstanceMapper.java
+++
@@ -1,9 +0,0 @@
-package com.github.pdaodao.springwebplus.bflow.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.github.pdaodao.springwebplus.bflow.entity.BfInstanceEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BfInstanceMapper extends BaseMapper<BfInstanceEntity> {
-}
--- spring-web-plus-flow/src/main/java/com/github/pdaodao/springwebplus/bflow/mapper/BfTaskMapper.java
+++
@@ -1,9 +0,0 @@
-package com.github.pdaodao.springwebplus.bflow.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.github.pdaodao.springwebplus.bflow.entity.BfTaskEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BfTaskMapper extends BaseMapper<BfTaskEntity> {
-}
