8f673c648011440e1f70df9bf9ccbfd8322df824
apengda <damengpeng@163.com>
2026-01-20 08:46:09 +0800

    问答场景管理

--- spring-web-plus-ai-starter/src/main/java/com/github/pdaodao/springwebplus/ai/entity/AiChatApp.java
+++ spring-web-plus-ai-starter/src/main/java/com/github/pdaodao/springwebplus/ai/entity/AiChatApp.java
@@ -64,14 +64,6 @@ public class AiChatApp extends SnowIdWithTimeUserEntity implements WithTeam {
     @TableField(typeHandler = JacksonTypeHandler.class)
     private ChatAppConfig appConfig;
 
-    @Schema(description = "topK文档数")
-    @TableFieldSize(defaultValue = "5")
-    private Integer topK = 5;
-
-    @Schema(description = "相似度得分")
-    @TableFieldSize(defaultValue = "0.6")
-    private Double score = 0.6;
-
     @TableFieldIndex
     @Schema(description = "团队-租户id")
     private String teamId;
--- spring-web-plus-ai-starter/src/main/java/com/github/pdaodao/springwebplus/ai/pojo/ChatAppConfig.java
+++ spring-web-plus-ai-starter/src/main/java/com/github/pdaodao/springwebplus/ai/pojo/ChatAppConfig.java
@@ -1,5 +1,6 @@
 package com.github.pdaodao.springwebplus.ai.pojo;
 
+import com.github.pdaodao.springwebplus.base.frame.TableFieldSize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -9,5 +10,11 @@ public class ChatAppConfig {
     @Schema(description = "是否使用历史")
     private Boolean useHistory = true;
 
+    @Schema(description = "topK文档数")
+    @TableFieldSize(defaultValue = "5")
+    private Integer topK = 5;
 
+    @Schema(description = "相似度得分")
+    @TableFieldSize(defaultValue = "0.6")
+    private Double score = 0.6;
 }
