c407f6f1ba99cda5f73eed0ed09b07c6cc66e371
apengda <damengpeng@163.com>
2026-01-21 18:00:09 +0800

    Update WithVersion.java

--- spring-web-plus-base/src/main/java/com/github/pdaodao/springwebplus/base/entity/WithVersion.java
+++ spring-web-plus-base/src/main/java/com/github/pdaodao/springwebplus/base/entity/WithVersion.java
@@ -1,7 +1,5 @@
 package com.github.pdaodao.springwebplus.base.entity;
 
-import com.github.pdaodao.springwebplus.base.pojo.PublishStatus;
-
 public interface WithVersion extends Entity<String>{
     /**
      * 当前版本号
@@ -10,12 +8,4 @@ public interface WithVersion extends Entity<String>{
     Integer getVersion();
 
     void setVersion(Integer version);
-
-    /**
-     * 发布状态 草稿、发布、下线
-     * @return
-     */
-    PublishStatus getPublishStatus();
-
-    void setPublishStatus(PublishStatus publishStatus);
 }
