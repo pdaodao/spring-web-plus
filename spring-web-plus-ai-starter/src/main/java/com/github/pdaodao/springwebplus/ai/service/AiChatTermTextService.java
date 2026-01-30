b90a7d518ac995881f91631ae891e471d541c12c
apengda <damengpeng@163.com>
2026-01-30 11:57:49 +0800

    Update AiChatTermTextService.java

--- spring-web-plus-ai-starter/src/main/java/com/github/pdaodao/springwebplus/ai/service/AiChatTermTextService.java
+++ spring-web-plus-ai-starter/src/main/java/com/github/pdaodao/springwebplus/ai/service/AiChatTermTextService.java
@@ -5,12 +5,14 @@ import com.github.pdaodao.springwebplus.ai.AiVectorStore;
 import com.github.pdaodao.springwebplus.ai.base.AiChatNamespace;
 import com.github.pdaodao.springwebplus.ai.dao.AiChatTermTextDao;
 import com.github.pdaodao.springwebplus.ai.entity.AiChatTermText;
+import com.github.pdaodao.springwebplus.ai.pojo.AiChatContext;
 import com.github.pdaodao.springwebplus.ai.store.AiEmbedText;
 import com.github.pdaodao.springwebplus.ai.store.AiEmbedTextQuery;
 import com.github.pdaodao.springwebplus.ai.store.AiStoreEmbeddingUtil;
 import com.github.pdaodao.springwebplus.tool.util.Preconditions;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +28,35 @@ public class AiChatTermTextService {
 
     public List<AiChatTermText> infoList(final String teamId, final String q){
         return termTextDao.infoList(teamId, q);
+    }
+
+    /**
+     * 通过关键词到向量库中检索
+     */
+    public List<AiChatTermText> search(final String teamId, final String q) throws Exception{
+        if(vectorStore.isEmpty()){
+            return termTextDao.infoList(teamId, q);
+        }
+        final AiEmbedTextQuery query = new AiEmbedTextQuery();
+        query.addNamespace(AiChatNamespace.term.name());
+        query.setTeamId(teamId);
+        final AiChatContext context = AiChatContext.fromHolder();
+        if(context != null && context.getChatApp() != null){
+            query.setTopK(context.getChatApp().getAppConfig().getTopK());
+            query.setScore(context.getChatApp().getAppConfig().getScore());
+        }
+        AiStoreEmbeddingUtil.buildQuery(query, AiChatModelProvider.ofEmbedding(teamId, null));
+        final List<AiChatTermText> ret = new ArrayList<>();
+        final List<AiEmbedText> list = vectorStore.get().query(query);
+        for(final AiEmbedText h: list){
+            final AiChatTermText term = new AiChatTermText();
+            term.setId(h.getId());
+            term.setTeamId(h.getTeamId());
+            term.setTitle(h.getName());
+            term.setRemark(h.getContent());
+            ret.add(term);
+        }
+        return ret;
     }
 
     public void save(AiChatTermText text) throws Exception{
